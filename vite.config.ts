d986aba533b38937b7f2f2c46335e62c7ef2afcc
Hossein-ansari <hossein838485@gmail.com>
2025-05-20 19:47:11 -0700

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/madar-manteghi/',
   plugins: [preact()],
-  base: '/madar-manteghi/' // Add this line for GitHub Pages
+  build: {
+    outDir: 'dist'
+  }
 })
